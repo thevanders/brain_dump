89c803898c2e6ec34697e9bdccd9464a28c3c4a2
Mark <markvandermaas@gmail.com>
2016-11-14 10:37:24 +0100

    incorrectly used PROJECT_ROOT instead of BASE_DIR

--- braindump/settings.py
+++ braindump/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static ")]#, 'var/www/static/',]
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
