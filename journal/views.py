ae6b034da9b503d94bf84292402f41e446351474
Mark <markvandermaas@gmail.com>
2016-11-16 00:29:21 +0100

    refactor views, add header image to add_entry, attempting to do some css stuff...

--- journal/views.py
+++ journal/views.py
@@ -7,20 +7,22 @@ from .forms import *
 
 today = datetime.today().strftime("%Y-%m-%d")
 
+images = DailyImage.objects.get(date=today)
+if images.location:
+    name = images.name + ", " + images.location
+else:
+    name = images.name
+
 
 # Create your views here.
 def IndexView(request):
     journal_entries = JournalEntry.objects.order_by('-pk')
-    images = DailyImage.objects.get(date=today)
-    if images.location:
-        name = images.name + ", " + images.location
-    else:
-        name = images.name
     template = loader.get_template('journal/index.html')
     context = {
         'journal_entries': journal_entries,
         'images': images,
-        'name': name
+        'name': name,
+        'today': today,
     }
     return HttpResponse(template.render(context, request))
 
@@ -32,7 +34,8 @@ def EntryView(request):
         return HttpResponseRedirect(reverse('journal:index_view'))
     else:
         form = JournalEntryForm()
-
     return render(request, 'journal/add_entry.html', {
         'form': form,
+        'images': images,
+        'today': today,
     })
